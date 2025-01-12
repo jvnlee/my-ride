447d1104c452a63b112bc54e7b7d9613492a8545
jvnlee <lhjun1028@naver.com>
2025-01-13 05:43:42 +0900

    initial commit

---
+++ src/test/java/com/jvnlee/myride/MyRideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvnlee.myride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
