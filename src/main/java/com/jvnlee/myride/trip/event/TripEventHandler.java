944c2ec379f418b7acd1b406e29a729672ce46d4
jvnlee <lhjun1028@naver.com>
2025-01-17 06:06:05 +0900

    feat: TripEventHandler에 RiderWebSocketBroadcaster 의존성 추가

--- src/main/java/com/jvnlee/myride/trip/event/TripEventHandler.java
+++ src/main/java/com/jvnlee/myride/trip/event/TripEventHandler.java
@@ -1,20 +1,19 @@
 package com.jvnlee.myride.trip.event;
 
-import com.jvnlee.myride.rider.service.RiderBroadcastingService;
+import com.jvnlee.myride.rider.broadcaster.RiderWebSocketBroadcaster;
 import com.jvnlee.myride.trip.service.TripDriverAssignmentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class TripEventHandler {
 
     private final TripDriverAssignmentService tripDriverAssignmentService;
 
-    private final RiderBroadcastingService riderBroadcastingService;
+    private final RiderWebSocketBroadcaster riderWebSocketBroadcaster;
 
     @TransactionalEventListener
     public void handleTripCreated(TripCreatedEvent event) {
@@ -23,7 +22,7 @@ public class TripEventHandler {
 
     @TransactionalEventListener
     public void handleTripDriverAssigned(TripDriverAssignedEvent event) {
-        riderBroadcastingService.broadcastDriverLocation(event.getTripId(), event.getDriverId());
+        riderWebSocketBroadcaster.broadcastDriverLocation(event.getTripId(), event.getDriverId());
     }
 
 }
