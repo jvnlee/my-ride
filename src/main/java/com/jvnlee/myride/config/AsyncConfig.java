c7d9679c0a55256454c6d3349f91445082c0b599
jvnlee <lhjun1028@naver.com>
2025-01-17 06:53:05 +0900

    feat: AsyncConfig 추가
    시간 소요가 많은 일부 Eventhandler 메서드는 Async로 선언할 필요성이 있어서 비동기 쓰레드를 위한 configuration을 생성. 비동기 쓰레드에서 실행 중인 작업에서 예외가 발생하면 silent fail하는 경우가 생기므로 에러 로그를 위해 task decorator를 사용함. 특히 비동기 작업의 depth가 깊어지는 상황에서는 정확한 실행 흐름 분석이 필요하므로 발원지에서 예외를 되던지도록 설정함.

---
+++ src/main/java/com/jvnlee/myride/config/AsyncConfig.java
@@ -0,0 +1,31 @@
+package com.jvnlee.myride.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Slf4j
+@Configuration
+@EnableAsync
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("ASYNC-");
+
+        executor.initialize();
+        return executor;
+    }
+
+}
