171366fd5cd2ae9205e4c215057dfbb5261a70d6
jvnlee <lhjun1028@naver.com>
2025-01-17 16:54:42 +0900

    feat: 1대1 연관 관계의 Driver와 Vehicle에서 Driver가 FK를 가지도록 변경
    Vehicle은 Driver가 없어도 되지만, Driver는 반드시 Vehicle이 배정되어있어야 함.

--- src/main/java/com/jvnlee/myride/driver/Driver.java
+++ src/main/java/com/jvnlee/myride/driver/Driver.java
@@ -1,12 +1,16 @@
 package com.jvnlee.myride.driver;
 
+import com.jvnlee.myride.vehicle.Vehicle;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,7 +28,12 @@ public class Driver {
     @Column(nullable = false)
     private String name;
 
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vehicle_id", nullable = false)
+    private Vehicle vehicle;
+
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private DriverStatus status;
 
     public enum DriverStatus {
@@ -34,8 +43,9 @@ public class Driver {
     }
 
     @Builder
-    private Driver(String name) {
+    private Driver(String name, Vehicle vehicle) {
         this.name = name;
+        this.vehicle = vehicle;
         this.status = DriverStatus.AVAILABLE;
     }
 
--- src/main/java/com/jvnlee/myride/vehicle/Vehicle.java
+++ src/main/java/com/jvnlee/myride/vehicle/Vehicle.java
@@ -1,14 +1,10 @@
 package com.jvnlee.myride.vehicle;
 
-import com.jvnlee.myride.driver.Driver;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,10 +18,6 @@ public class Vehicle {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "driver_id")
-    private Driver driver;
 
     @Column(name = "model_name")
     private ModelName modelName;
@@ -42,10 +34,6 @@ public class Vehicle {
     private Vehicle(ModelName modelName, String licensePlateNumber) {
         this.modelName = modelName;
         this.licensePlateNumber = licensePlateNumber;
-    }
-
-    public void assignDriver(Driver driver) {
-        this.driver = driver;
     }
 
 }
